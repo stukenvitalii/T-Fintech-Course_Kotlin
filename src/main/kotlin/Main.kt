9114062f7d042be4017bee9297f77d01bc4a1c07
stukenvitalii <stukenvitalii@gmail.com>
2024-09-16 12:58:56 +0300

    use DSL and save .csv and .md file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,42 @@ package org.tinkoff
 import org.tinkoff.client.KudaGoClient
 import org.tinkoff.client.getMostRatedNews
 import org.tinkoff.client.saveNews
+import org.tinkoff.dsl.readme
+import java.io.File
+import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 suspend fun main() {
     val client = KudaGoClient()
     val newsList = client.getNews()
 
-    newsList.forEach(::println)
-
     val period = LocalDate.of(2024, 1, 1)..LocalDate.of(2024, 12, 31)
-    val mostRatedNews = newsList.getMostRatedNews(20, period)
-
-    println("Total news: ${newsList.size}")
-    println("Most rated news: ${mostRatedNews.size}")
-
-    mostRatedNews.forEach {
-        println(it)
-    }
+    val mostRatedNews = getMostRatedNews(20, period)
 
     saveNews("src/main/resources/news.csv", newsList)
+
+    val readmeContent = readme {
+        header(level = 1) { +"Most rated news: " }
+
+        for (news in mostRatedNews) {
+            header(level = 2) { +news.title }
+            header(level = 3) { +"Description: ".plus(news.description!!) }
+            header(level = 4) {
+                +"Publication date: ".plus(
+                    LocalDateTime.ofInstant(Instant.ofEpochMilli(news.publicationDate * 1000), ZoneId.systemDefault())
+                        .toLocalDate().format(
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                    )
+                )
+            }
+            header(level = 4) { +"Favorites count: ".plus(news.favoritesCount!!) }
+            header(level = 4) { +"Comments count: ".plus(news.commentsCount!!) }
+            dividingLine()
+        }
+    }
+
+    File("README.md").writeText(readmeContent.toString())
 }
